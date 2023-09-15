dd8868c40a687e41965528e2bdd349be86622973
Merlinkk <anshulkashyap2504@gmail.com>
2023-09-15 12:58:09 +0530

    updated comments

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ while num> 0:  # // 10 ** (k - 1)                       # using while loop here
         product = product * (digits % 10)               
         if product == 0:
             break
-        digits //= 10                                   #flooring the value of num one digit at a time 
+        digits = digits // 10                                   #flooring the value of num one digit at a time 
     print(largest, product)                             #Purely for reference 
     largest = max(largest, product)                     # using max() function to get maximum value in largest variable
-    num //= 10                                          #flooring the value of num, eliminating/deleting digits one at a time>> LOOP to 7
+    num = num // 10                                          #flooring the value of num, eliminating/deleting digits one at a time>> LOOP to 7
 
 print(largest)
 
